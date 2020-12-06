d070ddb2557cb4781b792caa646ae11c697f154b
Betsy Groton <grotoned@gmail.com>
2020-12-06 13:19:41 -0500

    feat: geolocation current weather and headlines from apis

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -1,7 +1,85 @@
-import '../style/App.css';
+/** @format */
 
-function CurrentWeather() {
-	return <div className='currentWeather'>Current Weathers</div>;
+import '../style/App.css';
+import React, { Component } from 'react';
+
+// require('dotenv').config();
+
+const api_weather = 'e707b58c89718134c069cbb85065ffc4';
+
+class CurrentWeather extends Component {
+	constructor() {
+		super();
+		this.state = {
+			//dummy data, coordinates for LIC
+			lat: 40.72683,
+			lng: -73.943512,
+		};
+		this.getWeather = this.getWeather.bind(this);
+		this.calculateTemp = this.calculateTemp.bind(this);
+		this.findMe = this.findMe.bind(this);
+	}
+
+	async findMe() {
+		try {
+			//find the user's location
+			navigator.geolocation.getCurrentPosition(async (position) => {
+				this.setState({
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				});
+			});
+			//should probably include something if the geolocation is not successful
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async getWeather() {
+		try {
+			//get their current weather data
+			const api_call = await fetch(
+				`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lng}&APPID=${api_weather}`
+			);
+
+			const data = await api_call.json();
+
+			this.setState({ data });
+		} catch (err) {
+			//need better error handling if the api key fails
+			console.log(err);
+		}
+	}
+
+	//temp conversion from kelvin
+	calculateTemp(degreesK, displayUnits) {
+		if (displayUnits === 'C') return Math.floor(degreesK - 273);
+		else return Math.floor(((degreesK - 273) * 9) / 5 + 32);
+	}
+
+	render() {
+		return (
+			<>
+				<div className="currentWeather">Current Weathers</div>
+
+				<p>1.</p>
+				<button onClick={this.findMe}>Find Me</button>
+				{/* probs also want a way for users to type location into a form */}
+
+				<p>2.</p>
+				<button onClick={this.getWeather}>Get Weather</button>
+
+				{this.state.data ? (
+					<>
+						<p>The weather in {this.state.data.name} is:</p>
+						<p>{this.calculateTemp(this.state.data.main.temp, 'F')} degrees</p>
+					</>
+				) : (
+					''
+				)}
+			</>
+		);
+	}
 }
 
 export default CurrentWeather;
--- src/Components/Headlines.js
+++ src/Components/Headlines.js
@@ -1,7 +1,43 @@
-import '../style/App.css';
+/** @format */
 
-function Headlines() {
-	return <div className='headlines'>Headlines</div>;
+import '../style/App.css';
+import React, { Component } from 'react';
+// require('dotenv').config();
+
+const api_news = 'ea82cee3b4344c309e5736689b7aef0e';
+
+class Headlines extends Component {
+	constructor() {
+		super();
+		this.getTopEnvironmentHeadlines = this.getTopEnvironmentHeadlines.bind(
+			this
+		);
+	}
+
+	async getTopEnvironmentHeadlines() {
+		try {
+			//get top environment news articles using NewsAPI
+			const api_call = await fetch(
+				`https://newsapi.org/v2/top-headlines?q=climate&apiKey=${api_news}`
+			);
+			let enviroNews = await api_call.json();
+			this.setState({ enviroNews });
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	render() {
+		console.log(this.state);
+		return (
+			<>
+				<div className="headlines">Headlines</div>
+				<button onClick={this.getTopEnvironmentHeadlines}>
+					Get Top Enviro News
+				</button>
+			</>
+		);
+	}
 }
 
 export default Headlines;

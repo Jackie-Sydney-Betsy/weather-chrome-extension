3912fb26a45f34dc5b9a1a56400dc63aac440407
Betsy Groton <grotoned@gmail.com>
2020-12-06 19:29:22 -0500

    feat: text field to input a city

--- src/Components/App.js
+++ src/Components/App.js
@@ -13,8 +13,12 @@ class App extends Component {
 			//dummy data, coordinates for LIC
 			lat: 40.72683,
 			lng: -73.943512,
+			city: '',
+			// state: '',
 		};
 		this.findMe = this.findMe.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	async findMe() {
@@ -32,11 +36,54 @@ class App extends Component {
 		}
 	}
 
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
+
+	handleSubmit(e) {
+		let city = this.state.cityInput;
+		// let state = this.state.stateInput;
+		e.preventDefault();
+		this.setState({
+			cityInput: '',
+			// stateInput: '',
+			city: city,
+			// state: state,
+		});
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<button onClick={this.findMe}>Find Me</button>
-				<CurrentWeather lat={this.state.lat} lng={this.state.lng} />
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="cityInput">
+						<small>City</small>
+					</label>
+					<input
+						name="cityInput"
+						type="text"
+						value={this.state.cityInput}
+						onChange={this.handleChange}
+						required
+					/>
+					{/* <label htmlFor="stateInput">
+						<small>State</small>
+					</label>
+					{/* make this a state dropdown */}
+					{/* <input
+						name="stateInput"
+						type="text"
+						value={this.state.stateInput}
+						onChange={this.handleChange}
+						required
+					/> */}{' '}
+
+					<button type="submit">Submit</button>
+				</form>
+				<CurrentWeather location={this.state} />
 				<WeatherHistory />
 				<Headlines />
 			</div>
--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -6,25 +6,32 @@ import React, { Component } from 'react';
 // require('dotenv').config();
 
 const api_weather = 'e707b58c89718134c069cbb85065ffc4';
+const api_weather2 = 'e9446f061bceb277c12cd5a91fe25e16';
 
 class CurrentWeather extends Component {
 	constructor(props) {
 		super(props);
-
 		this.getWeather = this.getWeather.bind(this);
 		this.calculateTemp = this.calculateTemp.bind(this);
 	}
 
 	async getWeather() {
 		try {
-			//get their current weather data
-			const api_call = await fetch(
-				`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lng}&APPID=${api_weather}`
-			);
-
-			const data = await api_call.json();
-
-			this.setState({ data });
+			//if the user input a city/state
+			if (this.props.location.city.length) {
+				const api_call = await fetch(
+					`https://api.openweathermap.org/data/2.5/weather?q=${this.props.location.city}&APPID=${api_weather2}`
+				);
+				const data = await api_call.json();
+				this.setState({ data });
+			} else {
+				//get their current weather based on lat/lng
+				const api_call = await fetch(
+					`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.location.lat}&lon=${this.props.location.lng}&APPID=${api_weather}`
+				);
+				const data = await api_call.json();
+				this.setState({ data });
+			}
 		} catch (err) {
 			//need better error handling if the api key fails
 			console.log(err);
@@ -38,6 +45,7 @@ class CurrentWeather extends Component {
 	}
 
 	render() {
+		console.log(this.props.location);
 		return (
 			<>
 				<div className="currentWeather">Current Weathers</div>

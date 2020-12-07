75550b5ea3f4844114b8a1f723afe251fdce8040
Sydney Scott <sydney.roxana@gmail.com>
2020-12-07 12:37:17 -0500

    Two Victory chart options

--- src/Components/WeatherHistory.js
+++ src/Components/WeatherHistory.js
@@ -1,7 +1,12 @@
 import '../style/App.css';
+import Chart from './Chart'
+import ChartAnimated from './ChartAnimated'
 
 function WeatherHistory() {
-	return <div className='weatherHistory'>Weather History</div>;
+	return (
+		<><div className='weatherHistory'>Weather History</div>
+		<ChartAnimated /></>
+	)
 }
 
 export default WeatherHistory;
